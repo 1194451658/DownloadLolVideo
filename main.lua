c4a8c838281e8142a63771892de980cd3db18d82
wanzhang <1194451658@qq.com>
2016-06-05 22:01:05 +0800

    测试下载lol英雄介绍界面

--- main.lua
+++ main.lua
@@ -3,11 +3,11 @@
 --	set lua path
 ---------------------
 
-print(string.format("Default path: %s", package.path))
+-- print(string.format("Default path: %s", package.path))
 -- print(string.format("Default cpath: %s", package.cpath))
 
 -- cur working directory
--- os.execute("dir")
+-- os.execute("echo %CD%")
 
 local path = {
 	".\\?.lua",
@@ -34,8 +34,24 @@ print("Download Lol Video From Official Website")
 -------------------------------------------------------------------------------
 -- 			check luasocket 
 -------------------------------------------------------------------
-socket = require "socket"
-print("luasocket version:", socket._VERSION)
+
+-- socket = require "socket"
+-- print("luasocket version:", socket._VERSION)
+-- local testWebsite = "http://www.baidu.com"
+-- print(http.request(testWebsite))
+
+http = require("socket.http")
+
+local lolWebsite = "http://gameinfo.euw.leagueoflegends.com/en/game-info/champions/"
+local tmpSavePath = ".\\champions.html"
+
+local lolWebPage = http.request(lolWebsite)
+local tmpFile, errorMsg = io.open(tmpSavePath, 'w')
+if tmpFile then
+	tmpFile:write(lolWebPage)
+end
+
+
 
 
 
